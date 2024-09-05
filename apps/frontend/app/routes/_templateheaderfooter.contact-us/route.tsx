8967aaa4b0ec4cfbd4ad335160874f2536ff999c
Your Name <you@example.com>
2024-09-05 05:34:47 +0300

    contactus page is completed !

--- apps/frontend/app/routes/_templateheaderfooter.contact-us/route.tsx
+++ apps/frontend/app/routes/_templateheaderfooter.contact-us/route.tsx
@@ -1,8 +1,11 @@
 import ContactUsPage from "./ContactUs";
 export default function Layout() {
-	return (
-		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-			<ContactUsPage />
-		</div>
-	);
+  return (
+    <div
+      className="container"
+      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}
+    >
+      <ContactUsPage />
+    </div>
+  );
 }
