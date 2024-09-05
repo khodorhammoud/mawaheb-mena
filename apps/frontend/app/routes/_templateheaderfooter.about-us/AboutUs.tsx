1b0ea93622bb42001ea7c9e44c7ed01f753b7cbf
khodorhammoud94@gmail.com <khodorhammoud94@gmail.com>
2024-09-05 09:01:50 +0200

    fixed file naming issues

--- apps/frontend/app/routes/_templateheaderfooter.about-us/AboutUs.tsx
+++ apps/frontend/app/routes/_templateheaderfooter.about-us/AboutUs.tsx
@@ -1,5 +1,5 @@
 import LayoutContainer from "../../common/layout_container";
-import Achievements from "../_templateheaderfooter.for-freelancers/achievements";
+import Achievements from "../_templateheaderfooter.for-freelancers/Achievements";
 import Topic from "./topic";
 import MoreAboutMawaheb from "./moreaboutmawaheb";
 import Swiper from "./swiper";
