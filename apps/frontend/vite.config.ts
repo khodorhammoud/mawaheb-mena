a46a28ea26ffb08af7fd4c20bc303cbeb18a3097
khodorhammoud94@gmail.com <khodorhammoud94@gmail.com>
2024-10-22 10:27:30 +0200

    changing file naming structure

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -10,6 +10,31 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
+      routes(defineRoutes) {
+        return defineRoutes((route) => {
+          route(
+            "dashboard",
+            "routes/_templatedashboard.dashboard/route.tsx",
+            () => {
+              // route("", "concerts/home.tsx", { index: true });
+              route(
+                "jobs",
+                "routes/_templatedashboard.dashboard/jobs/JobsList.tsx",
+                () => {
+                  route(
+                    ":jobId",
+                    "routes/_templatedashboard.dashboard/jobs/JobDetails.tsx"
+                  );
+                  route(
+                    "new",
+                    "routes/_templatedashboard.dashboard/jobs/NewJob.tsx"
+                  );
+                }
+              );
+            }
+          );
+        });
+      },
     }),
     tsconfigPaths(),
   ],
