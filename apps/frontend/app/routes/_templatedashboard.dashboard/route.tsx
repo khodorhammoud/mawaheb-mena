349519b4b28ec0841e55b4c6d7211eca72bfb567
khodorhammoud94@gmail.com <khodorhammoud94@gmail.com>
2024-10-25 07:11:55 +0200

    added useFetcher to onboarding page plus success/error message handling fixes

--- apps/frontend/app/routes/_templatedashboard.dashboard/route.tsx
+++ apps/frontend/app/routes/_templatedashboard.dashboard/route.tsx
@@ -1,11 +1,17 @@
-import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  json,
+  redirect,
+} from "@remix-run/node";
 import {
   getCurrentEployerFreelancerInfo,
   getCurrentUserAccountType,
+  getCurrentUser,
 } from "../../servers/user.server";
 import EmployerDashboard from "./employer";
 import FreelancerDashboard from "./freelancer/Dashboard";
-import { useLoaderData, useActionData, Form } from "@remix-run/react";
+import { useLoaderData /* useActionData, Form */ } from "@remix-run/react";
 import { AccountType } from "../../types/enums";
 import {
   getAllIndustries,
@@ -23,11 +29,9 @@ import {
   updateOnboardingStatus,
 } from "~/servers/employer.server";
 import { Employer } from "~/types/User";
-import { redirect } from "@remix-run/node";
-import { db } from "../../db/drizzle/connector"; // Import your db instance
-import { UsersTable } from "../../db/drizzle/schemas/schema"; // Adjust the path to where you define your schema
-import { eq } from "drizzle-orm"; // Import 'eq' for comparison
-import { getCurrentUser } from "../../servers/user.server";
+// import { db } from "../../db/drizzle/connector"; // Import your db instance
+// import { UsersTable } from "../../db/drizzle/schemas/schema"; // Adjust the path to where you define your schema
+// import { eq } from "drizzle-orm"; // Import 'eq' for comparison
 
 // Action
 // Action
@@ -44,87 +48,83 @@ export async function action({ request }: ActionFunctionArgs) {
       request
     )) as Employer;
 
-    switch (target) {
-      // ABOUT
-      case "employer-about":
-        const aboutContent = formData.get("about") as string;
-        const aboutStatus = await updateEmployerAbout(employer, aboutContent);
-        return json({ success: aboutStatus.success });
-
-      // BIO
-      case "employer-bio":
-        const bio = {
-          firstName: formData.get("firstName") as string,
-          lastName: formData.get("lastName") as string,
-          location: formData.get("location") as string,
-          websiteURL: formData.get("website") as string,
-          socialMediaLinks: {
-            linkedin: formData.get("linkedin") as string,
-            github: formData.get("github") as string,
-            gitlab: formData.get("gitlab") as string,
-            dribbble: formData.get("dribbble") as string,
-            stackoverflow: formData.get("stackoverflow") as string,
-          },
-          // userId: employer.account?.user?.id, // Ensure this is added
-          userId: userId,
-        };
-        const bioStatus = await updateEmployerBio(bio, employer);
-        return json({ success: bioStatus.success });
-
-      // INDUSTRIES
-      case "employer-industries":
-        const industries = formData.get("employer-industries") as string;
-        const industriesIds = industries
-          .split(",")
-          .map((industry) => parseInt(industry));
-        const industriesStatus = await updateEmployerIndustries(
-          employer,
-          industriesIds
-        );
-        return json({ success: industriesStatus.success });
-
-      // YEARS IN BUSINESS
-      case "employer-years-in-business":
-        const yearsInBusiness =
-          parseInt(formData.get("years-in-business") as string) || 0;
-        const yearsStatus = await updateEmployerYearsInBusiness(
-          employer,
-          yearsInBusiness
-        );
-        return json({ success: yearsStatus.success });
-
-      // BUDGET
-      case "employer-budget":
-        const budgetValue = formData.get("budget");
-        const budget = parseInt(budgetValue as string, 10);
-
-        const budgetStatus = await updateEmployerBudget(employer, budget);
-        return json({ success: budgetStatus.success });
-
-      // ONBOARDING -> TRUE ✅
-      case "employer-onboard":
-        const userExists = await checkUserExists(userId);
-        if (!userExists.length)
-          return json(
-            { success: false, error: { message: "User not found." } },
-            { status: 404 }
-          );
-
-        const result = await updateOnboardingStatus(userId);
-        return result.length
-          ? redirect("/dashboard")
-          : json(
-              {
-                success: false,
-                error: { message: "Failed to update onboarding status" },
-              },
-              { status: 500 }
-            );
-
-      // DEFAULT
-      default:
-        throw new Error("Unknown target update");
+    // ABOUT
+    if (target == "employer-about") {
+      const aboutContent = formData.get("about") as string;
+      const aboutStatus = await updateEmployerAbout(employer, aboutContent);
+      return json({ success: aboutStatus.success });
     }
+    // BIO
+    if (target == "employer-bio") {
+      const bio = {
+        firstName: formData.get("firstName") as string,
+        lastName: formData.get("lastName") as string,
+        location: formData.get("location") as string,
+        websiteURL: formData.get("website") as string,
+        socialMediaLinks: {
+          linkedin: formData.get("linkedin") as string,
+          github: formData.get("github") as string,
+          gitlab: formData.get("gitlab") as string,
+          dribbble: formData.get("dribbble") as string,
+          stackoverflow: formData.get("stackoverflow") as string,
+        },
+        userId: userId,
+      };
+      const bioStatus = await updateEmployerBio(bio, employer);
+      return json({ success: bioStatus.success });
+    }
+    // INDUSTRIES
+    if (target == "employer-industries") {
+      const industries = formData.get("employer-industries") as string;
+      const industriesIds = industries
+        .split(",")
+        .map((industry) => parseInt(industry));
+      const industriesStatus = await updateEmployerIndustries(
+        employer,
+        industriesIds
+      );
+      return json({ success: industriesStatus.success });
+    }
+    // YEARS IN BUSINESS
+    if (target == "employer-years-in-business") {
+      const yearsInBusiness =
+        parseInt(formData.get("years-in-business") as string) || 0;
+      const yearsStatus = await updateEmployerYearsInBusiness(
+        employer,
+        yearsInBusiness
+      );
+      return json({ success: yearsStatus.success });
+    }
+    // BUDGET
+    if (target == "employer-budget") {
+      const budgetValue = formData.get("budget");
+      const budget = parseInt(budgetValue as string, 10);
+
+      const budgetStatus = await updateEmployerBudget(employer, budget);
+      return json({ success: budgetStatus.success });
+    }
+    // ONBOARDING -> TRUE ✅
+    if (target == "employer-onboard") {
+      const userExists = await checkUserExists(userId);
+      if (!userExists.length)
+        return json(
+          { success: false, error: { message: "User not found." } },
+          { status: 404 }
+        );
+
+      const result = await updateOnboardingStatus(userId);
+      return result.length
+        ? redirect("/dashboard")
+        : json(
+            {
+              success: false,
+              error: { message: "Failed to update onboarding status" },
+            },
+            { status: 500 }
+          );
+    }
+    // DEFAULT
+    throw new Error("Unknown target update");
   } catch (error) {
     return json(
       { success: false, error: { message: "An unexpected error occurred." } },
@@ -132,10 +132,6 @@ export async function action({ request }: ActionFunctionArgs) {
     );
   }
 }
-
-// Loader
-// Loader
-// Loader
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const accountType: AccountType = await getCurrentUserAccountType(request);
