d19e59a6efb9c1528f9d976b5d070fe2fd79e6d5
Wassim Taleb <you@example.com>
2025-02-08 19:33:17 +0200

    clear all filters added, and the x icon is edited also

--- apps/frontend/app/routes/_templatedashboard.browse-jobs/filters/HourlyRateFilter.tsx
+++ apps/frontend/app/routes/_templatedashboard.browse-jobs/filters/HourlyRateFilter.tsx
@@ -82,7 +82,7 @@ export default function HourlyRateFilter({
             <X
               size={32}
               className="text-gray-500 p-1 rounded-full transition 
-              group-hover:text-white hover:text-green-500"
+              group-hover:text-white hover:bg-gray-400"
               onClick={(e) => {
                 e.stopPropagation();
                 clearHourlyRate();
