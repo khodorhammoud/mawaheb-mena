3679be12921c76cfed19d5bbc9ca4ca719aa63d1
Wassim Taleb <you@example.com>
2025-03-20 06:24:16 +0200

    toaster added :)

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -8,35 +8,28 @@ import {
   useLoaderData,
   isRouteErrorResponse,
   useRouteError,
-} from "@remix-run/react";
-import {
-  LinksFunction,
-  LoaderFunctionArgs,
-  MetaFunction,
-} from "@remix-run/node";
-import stylesheet from "./tailwind.css?url";
-import appStylesheet from "./styles/app-global.css?url";
-import i18nServer, { localeCookie } from "~/lib/i18n.server";
-import { useChangeLanguage } from "remix-i18next/react";
-import { useTranslation } from "react-i18next";
+} from '@remix-run/react';
+import { LinksFunction, LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import stylesheet from './tailwind.css?url';
+import appStylesheet from './styles/app-global.css?url';
+import i18nServer, { localeCookie } from '~/lib/i18n.server';
+import { useChangeLanguage } from 'remix-i18next/react';
+import { useTranslation } from 'react-i18next';
+import { Toaster } from '~/components/ui/toaster';
 
-export const handle = { i18n: ["translation"] };
+export const handle = { i18n: ['translation'] };
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  // const locale = await i18nServer.getLocale(request);
-  // return Response.json(
-  //   { locale },
-  //   { headers: { "Set-Cookie": await localeCookie.serialize(locale) } }
-  // );
-  return {};
+  const locale = await i18nServer.getLocale(request);
+  return { locale };
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const loaderData = useRouteLoaderData<typeof loader>("root");
+  const loaderData = useRouteLoaderData<typeof loader>('root');
   return (
     // if loaderData is not null or empty, it will take the .locale property of the localeData
     // but if it is empty, "en" will be provided :)
-    <html lang={loaderData?.locale ?? "en"}>
+    <html lang={loaderData?.locale ?? 'en'}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -45,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Toaster />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -53,13 +47,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", as: "style", href: stylesheet },
-  { rel: "stylesheet", href: appStylesheet }, // shall these href's stay like that ???
+  { rel: 'stylesheet', as: 'style', href: stylesheet },
+  { rel: 'stylesheet', href: appStylesheet }, // shall these href's stay like that ???
 ];
 
 export const meta: MetaFunction = () => {
   const { t } = useTranslation();
-  return [{ title: t("siteTitle") }];
+  return [{ title: t('siteTitle') }];
 };
 
 export default function App() {
