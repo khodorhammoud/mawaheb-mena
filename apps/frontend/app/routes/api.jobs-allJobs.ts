a7ea7b3efc44b06b1c2d9b349de0085c74ef8f65
khodorhammoud94@gmail.com <khodorhammoud94@gmail.com>
2025-04-21 16:58:58 +0200

    temp

--- apps/frontend/app/routes/api.jobs-allJobs.ts
+++ apps/frontend/app/routes/api.jobs-allJobs.ts
@@ -1,8 +1,9 @@
 // this is the code that is targeted using the fetcher inside all jobs
 
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { getAllJobs } from "../servers/job.server";
-import { getCurrentProfileInfo } from "~/servers/user.server";
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { getAllJobs } from '../servers/job.server';
+import { getCurrentProfileInfo } from '~/servers/user.server';
+import { json } from '@remix-run/node';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const user = await getCurrentProfileInfo(request);
@@ -10,5 +11,5 @@ export async function loader({ request }: LoaderFunctionArgs) {
   // TODO: check user is published, and whether it's important for the user to be a freelancer or employer
 
   const allJobs = await getAllJobs();
-  return Response.json({ jobs: allJobs });
+  return json({ jobs: allJobs });
 }
