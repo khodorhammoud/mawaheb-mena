87b63e09328317acc3131ae45cbea21b91a9f4db
khodorhammoud94@gmail.com <khodorhammoud94@gmail.com>
2025-05-08 11:16:05 +0200

    updated deploy script to include environment variables as github secrets

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // Enable CORS to allow connections from the frontend (port 5173)
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [process.env.FRONTEND_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
