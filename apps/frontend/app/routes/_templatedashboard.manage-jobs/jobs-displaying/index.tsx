1fb730ad275f9840fb54a321901ffb2a5e5c84c3
Wassim Taleb <you@example.com>
2025-05-27 04:05:20 +0300

    when clicking on a button of the filtering buttons in manage-jobs, i will not see the title since it is clear that i click draft job button lets say

--- apps/frontend/app/routes/_templatedashboard.manage-jobs/jobs-displaying/index.tsx
+++ apps/frontend/app/routes/_templatedashboard.manage-jobs/jobs-displaying/index.tsx
@@ -198,9 +198,6 @@ export default function JobManagement({
           )
         ) : (
           <div className="lg:mt-10 md:mt-12 mt-16">
-            <h2 className="font-semibold xl:mb-10 mb-8 xl:text-3xl lg:text-2xl text-2xl ml-1">
-              {capitalize(activeFilter)} Jobs
-            </h2>
             <div className={getGridClass(viewMode)}>
               {pagedJobs.map(jobCardData => (
                 <Job
