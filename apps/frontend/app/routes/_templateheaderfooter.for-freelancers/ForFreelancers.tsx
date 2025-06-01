2bc8c0fb604087592e86636aae11be5bc0883ee9
Wassim Taleb <you@example.com>
2025-06-01 14:36:58 +0300

    scrolling in the main page is fixed idk :D

--- apps/frontend/app/routes/_templateheaderfooter.for-freelancers/ForFreelancers.tsx
+++ apps/frontend/app/routes/_templateheaderfooter.for-freelancers/ForFreelancers.tsx
@@ -1,15 +1,16 @@
 // i edit here and as the components i'll make in the comming days
 
-import "../../styles/wavy/wavy.css";
-import LayoutContainer from "../../common/layout_container";
-import HowItWorks from "../_templateheaderfooter.for-employers/howitworks/HowItWorks";
-import Segments from "../_templateheaderfooter.for-employers/Segments";
-import Languages from "../_templateheaderfooter.for-employers/Languages";
-import WhyWorkWithUs from "../_templateheaderfooter.for-employers/WhyWorkWithUs";
-import FAQ from "../_templateheaderfooter.for-employers/FAQ";
-import Topic from "./Topic";
-import Jobs from "./Jobs";
-import Achievements from "./Achievements";
+import '../../styles/wavy/wavy.css';
+import LayoutContainer from '../../common/layout_container';
+import HowItWorks from '../_templateheaderfooter.for-employers/howitworks/HowItWorks';
+import Segments from '../_templateheaderfooter.for-employers/Segments';
+import Languages from '../_templateheaderfooter.for-employers/Languages';
+import WhyWorkWithUs from '../_templateheaderfooter.for-employers/WhyWorkWithUs';
+import FAQ from '../_templateheaderfooter.for-employers/FAQ';
+import Topic from './Topic';
+import Jobs from './Jobs';
+import Achievements from './Achievements';
+import SegmentsLanguagesZoomingText from '../_templateheaderfooter.for-employers/SegmentsLanguagesZoomingText';
 
 export default function ForFreelancersPage() {
   return (
@@ -22,7 +23,8 @@ export default function ForFreelancersPage() {
         <Achievements />
       </div>
       <HowItWorks />
-      <Languages />
+      {/* <Languages /> */}
+      <SegmentsLanguagesZoomingText />
       <WhyWorkWithUs />
       <div className="mb-20">
         <FAQ />
