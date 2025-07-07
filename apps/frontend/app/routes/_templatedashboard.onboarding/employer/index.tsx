4aab4a7fc30d007ce1541c4cb69cfdd581ab33b1
Wassim Taleb <you@example.com>
2025-07-08 01:15:01 +0300

    removed-rating-from-the-onboarding-pages

--- apps/frontend/app/routes/_templatedashboard.onboarding/employer/index.tsx
+++ apps/frontend/app/routes/_templatedashboard.onboarding/employer/index.tsx
@@ -31,17 +31,17 @@ export default function EmployerOnboardingScreen() {
         }}
       >
         <div className="absolute top-4 right-4">
-          <button className="underline-none text-sm rounded-xl flex items-center justify-center text-primaryColor border border-gray-300 sm:px-5 sm:py-3 px-3 py-2 font-semibold tracking-wide not-active-gradient hover:text-white w-fit">
+          {/* <button className="underline-none text-sm rounded-xl flex items-center justify-center text-primaryColor border border-gray-300 sm:px-5 sm:py-3 px-3 py-2 font-semibold tracking-wide not-active-gradient hover:text-white w-fit">
             Add Title
-          </button>
+          </button> */}
           <div className="xl:right-40 lg:right-32 md:right-24 sm:right-16 right-10">
             {/* Conditionally Render Star Rating */}
-            {!accountOnboarded && (
+            {/* {!accountOnboarded && (
               <div className="flex items-center justify-end mt-6 mr-1">
                 <AiFillStar className="text-yellow-500 h-5 w-5 mr-1" />
                 <span>0/5</span>
               </div>
-            )}
+            )} */}
           </div>
         </div>
       </div>
